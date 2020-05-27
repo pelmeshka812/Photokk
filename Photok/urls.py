4031ea58271e2e2af928b9e341f2f9a7b0bb6815
pelmeshka812 <pelmeshka812@gmail.com>
2020-05-27 21:41:31 +0300

    host fixes

--- Photok/urls.py
+++ Photok/urls.py
@@ -21,6 +21,9 @@ urlpatterns = [
 
 urlpatterns += doc_urls
 
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #handler404 = blog.views.error_404
