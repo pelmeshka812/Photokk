72af98219ee5679753c0d23a29da0228fd527d68
pelmeshka812 <pelmeshka812@gmail.com>
2020-05-27 21:47:03 +0300

    host fixes

--- Photok/settings.py
+++ Photok/settings.py
@@ -144,9 +144,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 #LOGIN_REDIRECT_URL = 'blog:song_list'
 LOGIN_URL = 'accounts:login'
