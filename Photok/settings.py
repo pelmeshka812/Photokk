4d46a82c03d32b59c0faef0ae1ed1b85126a4112
pelmeshka812 <pelmeshka812@gmail.com>
2020-05-28 12:01:42 +0300

    Templates changes

--- Photok/settings.py
+++ Photok/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
@@ -151,7 +151,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 #     os.path.join(BASE_DIR, "static"),
 # ]
 
-#LOGIN_REDIRECT_URL = 'blog:song_list'
 LOGIN_URL = 'accounts:login'
 LOGOUT_URL = 'accounts:logout'
 LOGOUT_REDIRECT_URL = 'accounts:login'
